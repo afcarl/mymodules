b762eb97ecc184736ea8eb98e234077b187039d0
Dan Tamayo <tamayo.daniel@gmail.com>
2016-04-11 08:47:10 -0400

    have cluster now do startup internally and return client

--- cluster/cluster.py
+++ cluster/cluster.py
@@ -1,12 +1,5 @@
 from subprocess import call
-
-def start_cluster(host, Nengines):
-	call("ssh -f {0} \"bash -tlc 'ipcluster start -n {1}'\"".format(host, Nengines), shell=True)
-
-def shutdown_cluster(host, client):
-	client.shutdown(hub=True)
-	call("ssh -f {0} \"bash -tlc 'ipcluster stop'\"".format(host), shell=True)
-	call("ssh -f {0} \"bash -tlc 'pkill -u dtamayo'\"".format(host), shell=True)
+from ipyparallel import Client
 
 bubbles = {'ipython_dir':'/cita/d/sunny-home/dtamayo/.ipython', 'sshserver':'bubbles'}
 ricky = {'ipython_dir':'/cita/d/sunny-home/dtamayo/.ipython', 'sshserver':'ricky'}
@@ -15,3 +8,15 @@ lobster = {'sshserver':'lobster'}
 kingcrab = {'sshserver':'kingcrab'}
 prawn = {'sshserver':'prawn'}
 trinity = {'sshserver':'trinity'}
+
+hosts = {'bubbles':bubbles, 'ricky':ricky, 'homard':homard, 'lobster':lobster, 'kingcrab':kingcrab, 'prawn':prawn, 'trinity':trinity}
+
+def start_cluster(host, Nengines):
+	call("ssh -f {0} \"bash -tlc 'ipcluster start -n {1}'\"".format(host, Nengines), shell=True)
+	return Client(**hosts[host])
+
+def shutdown_cluster(host, client):
+	client.shutdown(hub=True)
+	call("ssh -f {0} \"bash -tlc 'ipcluster stop'\"".format(host), shell=True)
+	call("ssh -f {0} \"bash -tlc 'pkill -u dtamayo'\"".format(host), shell=True)
+
